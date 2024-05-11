b2592bf97b5bffd73f8c17a7238d34e7da4c0330
xinzhao2627 <94624435+xinzhao2627@users.noreply.github.com>
2024-05-11 10:54:44 +0800

    RAINNAND removed ikonli, validator, bootstrapfx

--- DSA/src/main/java/module-info.java
+++ DSA/src/main/java/module-info.java
@@ -1,12 +1,6 @@
 module com.example.dsa {
     requires javafx.controls;
     requires javafx.fxml;
-
-    requires org.controlsfx.controls;
-    requires net.synedra.validatorfx;
-    requires org.kordamp.ikonli.javafx;
-    requires org.kordamp.bootstrapfx.core;
-
     opens com.example.dsa to javafx.fxml;
     exports com.example.dsa;
 }
